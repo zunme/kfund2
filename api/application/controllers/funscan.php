6a03c7ee868a7eda64eaf388da4a962237370cd8
zunme <zunme@nate.com>
2019-04-23 21:54:36 +0900

    ..

--- api/application/controllers/funscan.php
+++ api/application/controllers/funscan.php
@@ -35,7 +35,7 @@ class funscan extends CI_Controller {
       $data['mode']="ready";
       $this->db->insert('funscan', $data);
       $data['mode']="regist";
-
+      $data['reward']="";
     }else {
       $data = $qry->row_array();
       $data['mode']=($data['mode']=="ready")?"regist":"modify";
@@ -49,31 +49,32 @@ class funscan extends CI_Controller {
     var_dump($result);
   }
   function callAPI($url,$method="GET", $data=""){
-  $curl = curl_init();
-  #curl_setopt( $curl, CURLOPT_USERAGENT, $userAgent );
-  #curl_setopt( $curl, CURLOPT_REFERER,$referer);
-  switch ($method){
-    case "POST":
-       curl_setopt($curl, CURLOPT_POST, 1);
-       if (is_array($data) || $data!="" )
-          curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-       break;
-    case "PUT":
-       curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "PUT");
-       if (is_array($data) || $data!="" )
-          curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-       break;
-    default:
-       if (is_array($data) || $data!="" ) $url = sprintf("%s?%s", $url,http_build_query($data));
+    $curl = curl_init();
+    #curl_setopt( $curl, CURLOPT_USERAGENT, $userAgent );
+    #curl_setopt( $curl, CURLOPT_REFERER,$referer);
+    switch ($method){
+      case "POST":
+         curl_setopt($curl, CURLOPT_POST, 1);
+         if (is_array($data) || $data!="" )
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+         break;
+      case "PUT":
+         curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "PUT");
+         if (is_array($data) || $data!="" )
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+         break;
+      default:
+         if (is_array($data) || $data!="" ) $url = sprintf("%s?%s", $url,http_build_query($data));
+    }
+    // OPTIONS:
+    curl_setopt($curl, CURLOPT_URL, $url);
+    //curl_setopt($curl, CURLOPT_HTTPHEADER, array('APIKEY: 111111111111111111111','Content-Type: application/json',));
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    //curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+    // EXECUTE:
+    $result = curl_exec($curl);
+    if(!$result){return false;}
+    curl_close($curl);
+    return $result;
   }
-  // OPTIONS:
-  curl_setopt($curl, CURLOPT_URL, $url);
-  //curl_setopt($curl, CURLOPT_HTTPHEADER, array('APIKEY: 111111111111111111111','Content-Type: application/json',));
-  curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-  //curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-  // EXECUTE:
-  $result = curl_exec($curl);
-  if(!$result){return false;}
-  curl_close($curl);
-  return $result;
 }
