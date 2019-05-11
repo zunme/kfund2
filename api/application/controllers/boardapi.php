cd496c79b50b7be427f87ff8428f80045e73841e
zunme <zunme@nate.com>
2019-05-11 17:07:33 +0900

    p

--- api/application/controllers/boardapi.php
+++ api/application/controllers/boardapi.php
@@ -35,8 +35,8 @@ class boardapi extends CI_Controller {
     $config['base_url'] = '/api/index.php/boardapi/boardlist?table='.$this->input->get('table').'&per='.$perpage.'&cate='.$this->input->get('cate').'&target='.$target.'&search='.$this->input->get('search');
     $config['total_rows'] = isset($count['cnt']) && $count['cnt']>0 ? $count['cnt'] : 0;
     $config['per_page'] = $perpage;
-    $temppage = ($start == 0 ) ? 1 : ( $start%$perpage);
-    $config['num_links'] = ($temppage<3)? 5-$temppage : 2;
+    $temppage = ($start == 0 ) ? 1 : sprintf('%d',( $start/$perpage));
+    $config['num_links'] = ($temppage<3)? 5-(int)$temppage : 2;
     $config['page_query_string'] = true;
 
   $config['first_link'] =false;
