5d7bcb388bfaedf774567e0912f5ce3dcaf74a22
zunme <zunme@nate.com>
2019-06-15 17:17:59 +0900

    no message

--- api/application/controllers/Alim.php
+++ api/application/controllers/Alim.php
@@ -1,5 +1,5 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-class Consulting extends CI_Controller {
+class Alim extends CI_Controller {
   function index() {
     $data['list'] = $this->db->query("select * from z_alimitok where code='J0001' order by regdate desc")->result_array();
     $this->load->view('alim',$data)
