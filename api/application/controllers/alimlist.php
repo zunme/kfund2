f4d1a67b33084ad48940f22cb4f9c255ac8c6e29
zunme <zunme@nate.com>
2019-06-15 17:30:05 +0900

    no message

--- api/application/controllers/alimlist.php
+++ api/application/controllers/alimlist.php
@@ -1,6 +1,7 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 class Alimlist extends CI_Controller {
   function index(){
-
+    $data['list'] = $this->db->query("select * from z_alimitok where code='J0001' order by regdate desc")->result_array();
+    $this->load->view('alim',$data);
   }
 }
