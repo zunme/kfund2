860425860c273e441297924cab48aa8cecf2ef50
zunme <zunme@nate.com>
2019-07-02 16:32:09 +0900

    te

---
+++ api/application/views/late.php
@@ -0,0 +1,134 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title> 케이펀딩 투자후기 </title>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no"> 
+	<link rel="stylesheet" href="/css/style.css"/>
+	<script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+	<script src="js/script.js"></script>
+	<link href="https://fonts.googleapis.com/css?family=Nanum+Gothic&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+</head>
+<body>
+	<div class="pageheader">
+		<div class="center">
+			<div class="title ft">투자후기</div>
+		</div>
+	</div>
+	<div class="main ft">
+		<div class="container">
+			<div class="notice">
+			<i class="material-icons">create</i>
+			<p>케이펀딩 회원님들의 소중한 투자후기입니다.</p>
+			</div>
+			<div class="row">
+				<div class="card">
+					<div class="border">
+						<div class="card-image">
+							<img src="/img/1.png">
+						</div>
+						<div class="card-body ">
+							<div class="card-category">투자후기</div>
+							<div class="card-title">이자수익 15만원!! 이걸로 기저귀 대량 득템!!</div>
+							<div class="card-btn"><a>자세히보기</a></div>
+						</div>
+					</div>
+				</div>
+				<div class="card">
+					<div class="border">
+						<div class="card-image">
+							<img src="/img/2.png">
+						</div>
+						<div class="card-body">
+							<div class="card-category">투자후기</div>
+							<div class="card-title">케이펀딩은 투자자 보호를 위해 차주를 까다롭게 고릅니다!!!</div>
+							<div class="card-btn"><a>자세히보기</a></div>
+						</div>
+					</div>
+				</div>
+				<div class="card">
+					<div class="border">
+						<div class="card-image">
+							<img src="/img/3.png">
+						</div>
+						<div class="card-body">
+							<div class="card-category">투자후기</div>
+							<div class="card-title">기대이상으로 일찍 돌아와준 두 상품</div>
+							<div class="card-btn"><a>자세히보기</a></div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="card">
+					<div class="border">
+						<div class="card-image">
+							<img src="/img/1.png">
+						</div>
+						<div class="card-body">
+							<div class="card-category">투자후기</div>
+							<div class="card-title">이자수익 15만원!! 이걸로 기저귀 대량 득템!!</div>
+							<div class="card-btn"><a>자세히보기</a></div>
+						</div>
+					</div>
+				</div>
+				<div class="card">
+					<div class="border">
+						<div class="card-image">
+							<img src="/img/2.png">
+						</div>
+						<div class="card-body">
+							<div class="card-category">투자후기</div>
+							<div class="card-title">케이펀딩은 투자자 보호를 위해 차주를 까다롭게 고릅니다!!!</div>
+							<div class="card-btn"><a>자세히보기</a></div>
+						</div>
+					</div>
+				</div>
+				<div class="card">
+					<div class="border">
+						<div class="card-image">
+							<img src="/img/3.png">
+						</div>
+						<div class="card-body">
+							<div class="card-category">투자후기</div>
+							<div class="card-title">기대이상으로 일찍 돌아와준 두 상품</div>
+							<div class="card-btn"><a>자세히보기</a></div>
+						</div>
+					</div>
+				</div>
+				<div class="center ft">
+					<ul class="pagination">
+						<li class="arrow"><a href="#">&lt;&lt;</a></li>
+						<li class="arrow"><a href="#">&lt;</a></li>
+						<li><a href="#">1</a></li>
+						<li><a href="#">2</a></li>
+						<li class="circle"><a href="#">3</a></li>
+						<li><a href="#">4</a></li>
+						<li><a href="#">5</a></li>
+						<li class="arrow"><a href="#">&gt;</a></li>
+						<li class="arrow"><a href="#">&gt;&gt;</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div class="black_bg">
+			<div class="join">
+				<div class="icon">
+					<img src="/img/a.png">
+				</div>
+				<p>아직 케이펀딩의 회원이 아니신가요?</p>
+				<div class="black_btn"><a href="#">회원가입 바로가기</a></div>
+			</div>
+			<div class="invest ">
+				<div class="icon">
+					<img src="/img/b.png">
+				</div>
+				<p>케이펀딩의 투자상품이 궁금하신가요?</p>
+				<div class="black_btn"><a href="#">투자상품 둘러보기</a></div>
+			</div>
+		</div>
+		<div class="subscribe-line"></div>
+	</div>
+</body>
+</html>
